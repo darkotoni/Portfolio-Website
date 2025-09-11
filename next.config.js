3110f2a43f77752a461e44dd4c12e384e703afe0
Darian <darkotan1000@gmail.com>
2025-09-10 17:06:17 -0700

    set up pages

--- next.config.js
+++ next.config.js
@@ -5,31 +5,11 @@ const nextConfig = {
   images: {
     unoptimized: true
   },
-  // Development optimizations
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
-      },
-    },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
-  // Optimize bundle
-  swcMinify: true,
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
-  },
-  // Webpack optimizations
-  webpack: (config, { dev }) => {
-    if (dev) {
-      config.watchOptions = {
-        poll: 1000,
-        aggregateTimeout: 300,
-      }
-    }
-    return config
   }
 }
 
